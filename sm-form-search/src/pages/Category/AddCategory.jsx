4135a73a367d0bc740fed9bf0d4b1fd4487b771e
Fatih Kurt <fatih.kurt@bigbrolis.com>
2023-01-13 16:21:34 +0200

    Empty user&category forms created

--- sm-form-search/src/pages/Category/AddCategory.jsx
+++ sm-form-search/src/pages/Category/AddCategory.jsx
@@ -1,12 +1,35 @@
-
+import { useState } from 'react'
 
 export default function AddCategory(props) {
-  const category = props.category || {};
-  return (
-    <div id="category">
-      <h1>Category Form</h1>
-      <div>
 
+  const [categoryName, setCategoryName] = useState("");
+
+  function handleCategoryNameChange(e) {
+    const catName = e.target.value;
+    setCategoryName(catName);
+  }
+
+  async function handleSaveCategory() {
+    // ...
+  }
+
+  return (
+    <div id="add-category">
+      <h1>Category Adding Form</h1>
+      <div>
+        <form className="app-form">
+          <label>
+            <span>Category Name</span>
+            <input 
+              type="text"
+              value={categoryName}
+              onChange={handleCategoryNameChange}
+            />
+          </label>
+          <p>
+            <button type="submit" onClick={handleSaveCategory}>Save</button>
+          </p>
+        </form>
       </div>
     </div>
   );
--- sm-form-search/src/pages/User/AddUser.jsx
+++ sm-form-search/src/pages/User/AddUser.jsx
@@ -1,10 +1,59 @@
+import { useState } from "react";
 
-export default function AddUser(props) {
-  const user = props.user || {};
+export default function AddUser() {
+  const [userForm, setUserForm] = useState({
+    fullname: "",
+    country: "",
+    city: "",
+    mail: "",
+  });
+
+  function setFormField(fieldName, fieldValue) {
+    userForm[fieldName] = fieldValue;
+    setUserForm({ ...userForm });
+  }
+
+  async function handleSaveUser() {
+    // ...
+  }
+
+  function renderTextField(fieldName) {
+    return (
+      <input
+        type="text"
+        value={userForm[fieldName]}
+        onChange={(e) => setFormField(fieldName, e.target.value)}
+      />
+    );
+  }
+
   return (
-    <div id="user">
+    <div id="add-user">
+      <h1>User Adding Form</h1>
       <div>
-        <h1>User Form</h1>
+        <form className="app-form">
+          <label>
+            <span>Full Name</span>
+            {renderTextField("fullname")}
+          </label>
+          <label>
+            <span>Country</span>
+            {renderTextField("country")}
+          </label>
+          <label>
+            <span>City</span>
+            {renderTextField("city")}
+          </label>
+          <label>
+            <span>E-mail</span>
+            {renderTextField("mail")}
+          </label>
+          <p>
+            <button type="button" onClick={handleSaveUser}>
+              Save
+            </button>
+          </p>
+        </form>
       </div>
     </div>
   );
